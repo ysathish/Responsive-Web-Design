c769e5e1f75b5bc5f83a92ef84f997eaf7ae6200
sathish <ysathish1234@gmail.com>
2024-01-02 00:56:42 +0530

    overflow and max/min widhth

---
+++ handilingOverflow.js
@@ -0,0 +1,11 @@
+// // Get a reference to the button element
+// var myButton = document.getElementById('myButton');
+
+// // Define the event listener function
+// function buttonClickHandler() {
+//     console.log("its working")
+//     alert('Button clicked!');
+// }
+
+// // Attach the event listener to the button
+// myButton.addEventListener('click', buttonClickHandler);
